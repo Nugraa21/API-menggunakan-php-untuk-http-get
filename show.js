8b4936312b2455394cbc4d016f1fab9907dbaad6
Nugra21 <ludang.prasetyo.2022@gmail.com>
2026-02-07 11:40:44 +0700

    -

--- show.js
+++ show.js
@@ -5,3 +5,6 @@ fetch("https://nonlitigious-alene-uninfinitely.ngrok-free.dev/presensi_rekap.php
 })
 .then(r => r.json())
 .then(console.log);
+
+
+// Penambahan keaman pada bagian Link api ( https://nonlitigious-alene-uninfinitely.ngrok-free.dev/presensi_rekap.php ) itu kan ada rekap php itu harusnya memunculkan json di web nya atau di console agar bisa di akses oleh Flutter tetapi ini di tambahkan block jadi datanya g langsung muncl jadi ada key unik untuk memunculkanya "X-App-Key": "Skaduta2025!@#SecureAPIKey1234567890" ini di pasang pada backend dan flutter 
